ecbefd00c4ebcab6a8dc95ec8c411717e491f6ad
rmjuarez12 <rmjuarez12@gmail.com>
2021-02-25 03:47:57 -0500

    Added a get endpoint for user

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -8,7 +8,7 @@ const jwt = require("jsonwebtoken");
 const secrets = require("../config/secrets");
 
 //* Import models
-const User = require("./users-model");
+//const User = require("./users-model");
 
 //* Import Middleware
 const validateUserBody = require("../middleware/validateUserBody");
@@ -17,92 +17,96 @@ const checkIfExists = require("../middleware/checkIfExists");
 
 //* Setup Endpoints
 
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "test" });
+});
+
 //-- [POST]
 // User Registration
-router.post(
-  "/register",
-  [validateUserBody.userReg, checkDuplicateRecords.users(User)],
-  (req, res) => {
-    const userData = req.body;
+// router.post(
+//   "/register",
+//   [validateUserBody.userReg, checkDuplicateRecords.users(User)],
+//   (req, res) => {
+//     const userData = req.body;
 
-    // Hash the password
-    const hash = bcrypt.hashSync(userData.password, 10);
-    userData.password = hash;
+//     // Hash the password
+//     const hash = bcrypt.hashSync(userData.password, 10);
+//     userData.password = hash;
 
-    User.createUser(userData)
-      .then((user) => {
-        res
-          .status(201)
-          .json({ message: "User has been created successfully!" });
-      })
-      .catch((err) => {
-        res
-          .status(500)
-          .json({ message: "Error registering user in", error: err.message });
-      });
-  }
-);
+//     User.createUser(userData)
+//       .then((user) => {
+//         res
+//           .status(201)
+//           .json({ message: "User has been created successfully!" });
+//       })
+//       .catch((err) => {
+//         res
+//           .status(500)
+//           .json({ message: "Error registering user in", error: err.message });
+//       });
+//   }
+// );
 
-// User Login
-router.post("/login", validateUserBody.userLogin, (req, res) => {
-  const { username, password } = req.body;
+// // User Login
+// router.post("/login", validateUserBody.userLogin, (req, res) => {
+//   const { username, password } = req.body;
 
-  User.getUserBy("username", username)
-    .then((user) => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateToken(user);
-        res
-          .status(200)
-          .json({ message: `Welcome ${user.name}`, userId: user.id, token });
-      } else {
-        res.status(403).json({ message: "Invalid username or password" });
-      }
-    })
-    .catch((err) => {
-      res
-        .status(500)
-        .json({ message: "Error logging user in", error: err.message });
-    });
-});
-
-//-- [GET]
-// Get a user by ID
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-
-  User.getUserBy("id", id)
-    .then((user) => {
-      res.status(200).json(user);
-    })
-    .catch((err) => {
-      res
-        .status(500)
-        .json({ message: "Error logging user in", error: err.message });
-    });
-});
-
-//-- [PUT]
-// User Edit
-// router.put("/:id", checkIfExists.users(User), (req, res) => {
-//   const {id} = req.params;
-
+//   User.getUserBy("username", username)
+//     .then((user) => {
+//       if (user && bcrypt.compareSync(password, user.password)) {
+//         const token = generateToken(user);
+//         res
+//           .status(200)
+//           .json({ message: `Welcome ${user.name}`, userId: user.id, token });
+//       } else {
+//         res.status(403).json({ message: "Invalid username or password" });
+//       }
+//     })
+//     .catch((err) => {
+//       res
+//         .status(500)
+//         .json({ message: "Error logging user in", error: err.message });
+//     });
 // });
 
-//-- [DELETE]
-// User Delete
-router.delete("/:id", checkIfExists.users(User), (req, res) => {
-  const { id } = req.params;
+// //-- [GET]
+// // Get a user by ID
+// router.get("/:id", (req, res) => {
+//   const { id } = req.params;
 
-  User.deleteUser(id)
-    .then(() => {
-      res.status(200).json({ message: "User has been deleted successfully!" });
-    })
-    .catch((err) => {
-      res
-        .status(500)
-        .json({ message: "Error logging user in", error: err.message });
-    });
-});
+//   User.getUserBy("id", id)
+//     .then((user) => {
+//       res.status(200).json(user);
+//     })
+//     .catch((err) => {
+//       res
+//         .status(500)
+//         .json({ message: "Error logging user in", error: err.message });
+//     });
+// });
+
+// //-- [PUT]
+// // User Edit
+// // router.put("/:id", checkIfExists.users(User), (req, res) => {
+// //   const {id} = req.params;
+
+// // });
+
+// //-- [DELETE]
+// // User Delete
+// router.delete("/:id", checkIfExists.users(User), (req, res) => {
+//   const { id } = req.params;
+
+//   User.deleteUser(id)
+//     .then(() => {
+//       res.status(200).json({ message: "User has been deleted successfully!" });
+//     })
+//     .catch((err) => {
+//       res
+//         .status(500)
+//         .json({ message: "Error logging user in", error: err.message });
+//     });
+// });
 
 //* Function to create the signature for the token
 function generateToken(user) {
